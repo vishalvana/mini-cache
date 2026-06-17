5d65692c3351bdb89cb1b957bc1a1388b4a60e0d
vishal vana <vishalvana592003@gmail.com>
2026-06-17 20:58:24 +0530

    Added Swagger OpenAPI Configuration for developer control

---
+++ src/main/java/com/vishal/mini_cache/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.vishal.mini_cache.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI cacheOpenAPI() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Mini Cache Engine")
+                                .version("1.0")
+                                .description(
+                                        "Custom Cache Implementation with TTL, Statistics and Cleanup Support"
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Vishal Vana")
+                                                .email("vishal@example.com")
+                                )
+                );
+    }
+}
--- src/main/java/com/vishal/mini_cache/controller/CacheController.java
+++ src/main/java/com/vishal/mini_cache/controller/CacheController.java
@@ -3,8 +3,14 @@ package com.vishal.mini_cache.controller;
 import com.vishal.mini_cache.dto.CacheRequest;
 import com.vishal.mini_cache.dto.StatsResponse;
 import com.vishal.mini_cache.service.CacheService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Cache API",
+        description = "Custom In-Memory Cache Operations"
+)
 @RestController
 @RequestMapping("/cache")
 public class CacheController {
@@ -15,6 +21,9 @@ public class CacheController {
         this.cacheService = cacheService;
     }
 
+    @Operation(
+            summary = "Store a value in cache"
+    )
     @PostMapping
     public String put(
             @RequestBody CacheRequest request) {
@@ -27,6 +36,9 @@ public class CacheController {
         return "Stored Successfully";
     }
 
+    @Operation(
+            summary = "Retrieve value by key"
+    )
     @GetMapping("/{key}")
     public Object get(
             @PathVariable String key) {
@@ -34,6 +46,9 @@ public class CacheController {
         return cacheService.get(key);
     }
 
+    @Operation(
+            summary = "Delete cache entry"
+    )
     @DeleteMapping("/{key}")
     public String delete(
             @PathVariable String key) {
@@ -43,11 +58,17 @@ public class CacheController {
         return "Deleted Successfully";
     }
 
+    @Operation(
+            summary = "Get cache size"
+    )
     @GetMapping("/size")
     public int size() {
         return cacheService.size();
     }
 
+    @Operation(
+            summary = "Get cache Summary Statistics"
+    )
     @GetMapping("/stats")
     public StatsResponse stats() {
         return cacheService.getStats();
